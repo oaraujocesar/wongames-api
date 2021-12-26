ef7c48c7d8ef43f71af883e5a640b1f239846777
César O. Araújo <oaraujocesar@gmail.com>
2021-12-26 14:41:26 -0300

    :tada: Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7f46297b531e13ff9f360147300af8bf'),
+    },
+  },
+});
