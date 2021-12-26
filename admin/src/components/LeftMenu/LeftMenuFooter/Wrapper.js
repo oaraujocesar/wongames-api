851d9f9bea297f173c993979bd76d81176bd9951
César O. Araújo <oaraujocesar@gmail.com>
2021-12-26 16:26:42 -0300

    style: :art: Change left menu footer style

---
+++ admin/src/components/LeftMenu/LeftMenuFooter/Wrapper.js
@@ -0,0 +1,58 @@
+import styled, { css } from "styled-components";
+import PropTypes from "prop-types";
+
+const Wrapper = styled.div`
+  ${({ theme }) => css`
+    position: absolute;
+    width: 100%;
+    background: ${theme.main.colors.won.blue};
+    bottom: 0;
+
+    .poweredBy {
+      width: 100%;
+      bottom: 0;
+      height: 3rem;
+      padding-left: 15px;
+      padding-right: 15px;
+      line-height: 3rem;
+      background-color: rgba(255, 255, 255, 0.02);
+      font-size: 1rem;
+      font-weight: 400;
+      letter-spacing: 0.05rem;
+      vertical-align: middle;
+      color: ${({ theme }) => theme.main.colors.strapi["gray-light"]};
+
+      a {
+        color: ${({ theme }) => theme.main.colors.won.orange};
+      }
+    }
+  `}
+`;
+
+const A = styled.a`
+  &:hover {
+    color: #007bff;
+    text-decoration: underline;
+  }
+`;
+
+Wrapper.defaultProps = {
+  theme: {
+    main: {
+      colors: {
+        strapi: {},
+      },
+      sizes: {
+        header: {},
+        leftMenu: {},
+      },
+    },
+  },
+};
+
+Wrapper.propTypes = {
+  theme: PropTypes.object,
+};
+
+export default Wrapper;
+export { A };
