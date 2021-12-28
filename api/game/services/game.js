96d271e0f933790bf65f6f3c3d67ef9ce1a9b048
César O. Araújo <oaraujocesar@gmail.com>
2021-12-28 20:11:37 -0300

    feat: :sparkles: Scrapp data from GOG

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -5,14 +5,31 @@
  * to customize this service
  */
 
-const axios = require('axios')
+const axios = require("axios");
+
+const getGameInfo = async (slug) => {
+  const jsDOM = require("jsdom"),
+    { JSDOM } = jsDOM,
+    { data } = await axios.get(`https://www.gog.com/game/${slug}`),
+    dom = new JSDOM(data);
+
+    const description = dom.window.document.querySelector('.description')
+
+    return {
+      rating: 'BR0',
+      short_description: description.textContent.slice(0, 160),
+      description: description.innerHTML
+    }
+};
 
 module.exports = {
   populate: async (params) => {
-    const gogApiURL = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
+    const gogApiURL = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
 
-    const { data: { products } } = await axios.get(gogApiURL)
+    const {
+      data: { products },
+    } = await axios.get(gogApiURL);
 
-    console.log(products[0])
+    console.log(await getGameInfo(products[0].slug));
   },
 };
