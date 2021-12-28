b9b02e9f461e68a0336e9f6a6d417885b195ee3a
César O. Araújo <oaraujocesar@gmail.com>
2021-12-28 19:35:22 -0300

    feat: :sparkles: Initialize the populate route

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,16 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (ctx) => {
+    console.log("Starting to populate...")
+
+    console.log(ctx.query)
+
+    ctx.send("Finished populating!");
+  },
+};
